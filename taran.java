e72387207a4c10f630519dbca7127810772c935c
taran3690 <tkaur3693@gmail.com>
2026-05-16 11:33:14 +0530

    Add chapter1 and chapter2 classes in taran.java

---
+++ taran.java
@@ -0,0 +1,14 @@
+chapter1
+ public class chapter1{
+    public static void main (String[]args){
+        String name="taran";
+        System.out.println(name);
+    }
+}
+chapter2
+  public class chapter2{
+    public static void main (String[]args){
+        String name="taran";
+        System.out.println("hello" + name);
+    }
+}
