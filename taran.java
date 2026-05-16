325164b5f5e953946d6aef1e1c9dc60d66a9c00a
taran3690 <tkaur3693@gmail.com>
2026-05-16 11:49:50 +0530

    Update taran.java

--- taran.java
+++ taran.java
@@ -12,3 +12,99 @@ chapter2
         System.out.println("hello" + name);
     }
 }
+chapter3
+ public class chapter3{
+    public static void main (String[]args){
+           System.out.println(1.1);
+        System.out.println(2);
+        System.out.println(30);
+        System.out.println(400);
+        System.out.println(5000);
+    }
+}
+chapter4
+ public class chapter4{
+ public static void main (String[]args){
+     int num=32;
+     int num2=23;
+     System.out.println(num+num2);
+ }
+} 
+  chapter5
+   public class chapter5{
+    public static void main (String[]args){
+        int num=45;
+        String name="taran";
+        float  floatnum =9.993f;
+        System.out.println(name);
+        System.out.println(num);
+        System.out. println(floatnum);
+        
+        
+        
+    }
+}
+chapter6
+ public class chapter6{
+    public static void main (String[]args){
+        int x=34;
+        int y=23;
+        int z=121;
+      System.out.println(x+y+z);  
+    }
+}
+chapter7
+ public class chapter7{
+    public static void main (String[]args){
+        int  x=32,y=32,z=32;
+        System.out.println(x+y+z);
+        
+    }
+}
+chapter8
+ public class chapter8{
+    public static void main (String[]args){
+        int x,y,z;
+        x=y=z=50;
+        System.out.println(x+y+z);
+    }
+}
+chapter9
+ public class chapter9{
+    public static void main (String[]args){
+        byte num =-128;
+        short num1=-32768;
+        long num2=-21231313133l;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
+chapter10
+ public class chapter10{
+    public static void main(String[]args){
+        boolean javafun=true;
+        boolean hello=false;
+        System.out.println(javafun);
+        System.out.println(hello);
+    }
+}
+chapter11
+ public class chapter11{
+    public static void main(String[]args){
+        int name =3231;
+        float name1=19.53f;
+        
+        System.out.println(name);
+        System.out.println(name1);
+    }
+    }
+chyapter12
+ public class chapter12{
+    public static void main(String[]args){
+       int length=4; 
+       int width=6;
+       int area;
+      System.out.print( area=length*width);
+    }
+}
